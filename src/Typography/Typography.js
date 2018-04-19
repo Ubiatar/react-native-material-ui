82cbaccfe5fb2c40c5fa8ec0b866246b47285584
Antonio <antonio.rinaldo89@gmail.com>
2018-04-19 15:25:38 +0200

    Typography updated

--- src/Typography/Typography.js
+++ src/Typography/Typography.js
@@ -1,65 +1,70 @@
 import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
+import React, {PureComponent} from 'react'
 import {
     Text
 } from 'react-native'
 
 const contextTypes = {
-  uiTheme: PropTypes.object.isRequired
+    uiTheme: PropTypes.object.isRequired
 }
 
 function getStyles(props, context) {
-  const { palette, fontFamily, typography } = context.uiTheme;
-  const { type } = props;
+    const {palette, fontFamily, typography} = context.uiTheme;
+    const {type} = props;
 
-  if(type === 'title')
-    return {
-        textColor: palette.textColor,
-        regularFont: fontFamily,
-        fontSize: typography.appBar.fontSize
-    }
-  if(type === 'text')
-    return {
-        textColor: palette.textColor,
-        regularFont: fontFamily
-    }
-   if(type === 'button')
-    return {
-        textColor: palette.primaryColor,
-        regularFont: fontFamily
-    }
-    if(type === 'tutorial')
-     return {
-        textColor: palette.textColor,
-        regularFont: fontFamily,
-        fontSize: typography.subheading.fontSize
-    }
+    if (type === 'title')
+        return {
+            textColor: palette.textColor,
+            regularFont: fontFamily,
+            fontSize: typography.appBar.fontSize
+        }
+    if (type === 'text')
+        return {
+            textColor: palette.textColor,
+            regularFont: fontFamily
+        }
+    if (type === 'button')
+        return {
+            textColor: palette.primaryColor,
+            regularFont: fontFamily
+        }
+    if (type === 'tutorial')
+        return {
+            textColor: palette.textColor,
+            regularFont: fontFamily,
+            fontSize: typography.subheading.fontSize
+        }
+    if (type === 'darkText')
+        return {
+            textColor: palette.darkPrimary,
+            regularFont: fontFamily
+        }
 }
 
 
 export default class Typography extends PureComponent {
-  static propTypes = {
-      ...Text.propTypes,
-      type: PropTypes.string,
-      style: Text.propTypes.style
-  }
+    static propTypes = {
+        ...Text.propTypes,
+        type: PropTypes.string,
+        style: Text.propTypes.style
+    }
 
-  static defaultProps = {
-      type: 'text'
-  }
+    static defaultProps = {
+        type: 'text'
+    }
 
-  render() {
+    render() {
 
-      const customStyles = getStyles(this.props, this.context)
+        const customStyles = getStyles(this.props, this.context)
 
-      let textStyle = {
-        color: customStyles.textColor,
-        fontWeight: customStyles.fontWeight,
-        fontFamily: customStyles.regularFont,
-        fontSize: customStyles.fontSize
-      }
-      return <Text {...this.props} style={[textStyle, this.props.style]}>{this.props.children}</Text>
-  }
+        let textStyle = {
+            color: customStyles.textColor,
+            fontWeight: customStyles.fontWeight,
+            fontFamily: customStyles.regularFont,
+            fontSize: customStyles.fontSize
+        }
+        return <Text {...this.props} style={[textStyle, this.props.style]}>{this.props.children}</Text>
+    }
 }
 
 Typography.contextTypes = contextTypes;
