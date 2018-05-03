163c00ff21dfa6cf117f3ac86f359a6ff4896b65
antoniogiordano <antonio.giordano@getapper.com>
2018-05-03 14:19:56 +0200

    Fix Select

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -24,37 +24,13 @@ import SelectIOS from './SelectIOS.ios'
 
 import SelectAndroid from './SelectAndroid.android'
 
-var ColorPropType = require('ColorPropType');
-
 var Platform = require('Platform');
 
 var React = require('React');
 
 const PropTypes = require('prop-types');
 
-var StyleSheetPropType = require('StyleSheetPropType');
-
-var TextStylePropTypes = require('TextStylePropTypes');
-
-var UnimplementedView = require('UnimplementedView');
-
-const ViewPropTypes = require('ViewPropTypes');
-
-var ViewStylePropTypes = require('ViewStylePropTypes');
-
-var itemStylePropType = StyleSheetPropType(TextStylePropTypes);
-
 import { View, Text } from 'react-native'
-
-var pickerStyleType = StyleSheetPropType({
-
-    ...ViewStylePropTypes,
-
-    color: ColorPropType,
-
-});
-
-
 
 var MODE_DIALOG = 'dialog';
 
@@ -73,56 +49,9 @@ class SelectItem extends React.Component<{
 
     value?: any,
 
-    color?: ColorPropType,
-
     testID?: string,
 
 }> {
-
-    static propTypes = {
-
-        /**
-
-         * Text to display for this item.
-
-         */
-
-        label: PropTypes.string.isRequired,
-
-        labelText: PropTypes.string,
-
-        /**
-
-         * The value to be passed to picker's `onValueChange` callback when
-
-         * this item is selected. Can be a string or an integer.
-
-         */
-
-        value: PropTypes.any,
-
-        /**
-
-         * Color of this item's text.
-
-         * @platform android
-
-         */
-
-        color: ColorPropType,
-
-        /**
-
-         * Used to locate the item in end-to-end tests.
-
-         */
-
-        testID: PropTypes.string,
-
-    };
-
-
-
     render() {
 
         // The items are not rendered directly
@@ -130,7 +59,6 @@ class SelectItem extends React.Component<{
         throw null;
 
     }
-
 }
 
 
@@ -205,98 +133,6 @@ class Select extends React.Component<{
 
     };
 
-
-
-    // $FlowFixMe(>=0.41.0)
-
-    static propTypes = {
-
-        ...ViewPropTypes,
-
-        style: pickerStyleType,
-
-        /**
-
-         * Value matching value of one of the items. Can be a string or an integer.
-
-         */
-
-        selectedValue: PropTypes.any,
-
-        /**
-
-         * Callback for when an item is selected. This is called with the following parameters:
-
-         *   - `itemValue`: the `value` prop of the item that was selected
-
-         *   - `itemPosition`: the index of the selected item in this picker
-
-         */
-
-        onValueChange: PropTypes.func,
-
-        /**
-
-         * If set to false, the picker will be disabled, i.e. the user will not be able to make a
-
-         * selection.
-
-         * @platform android
-
-         */
-
-        enabled: PropTypes.bool,
-
-        /**
-
-         * On Android, specifies how to display the selection items when the user taps on the picker:
-
-         *
-
-         *   - 'dialog': Show a modal dialog. This is the default.
-
-         *   - 'dropdown': Shows a dropdown anchored to the picker view
-
-         *
-
-         * @platform android
-
-         */
-
-        mode: PropTypes.oneOf(['dialog', 'dropdown']),
-
-        /**
-
-         * Style to apply to each of the item labels.
-
-         * @platform ios
-
-         */
-
-        itemStyle: itemStylePropType,
-
-        /**
-
-         * Prompt string for this picker, used on Android in dialog mode as the title of the dialog.
-
-         * @platform android
-
-         */
-
-        prompt: PropTypes.string,
-
-        /**
-
-         * Used to locate this view in end-to-end tests.
-
-         */
-
-        testID: PropTypes.string,
-
-    };
-
-
-
     render() {
 
         let {labelText} = this.props
@@ -327,7 +163,6 @@ class Select extends React.Component<{
         }
 
     }
-
 }
 
 
