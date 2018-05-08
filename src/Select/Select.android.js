5a94c45ebff69ea01157781a8d03bd266dfb15d1
androsanta <andreasantu@gmail.com>
2018-05-08 11:48:07 +0200

    select tested on android and ios

---
+++ src/Select/Select.android.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+
+import { string, array, func } from 'prop-types';
+
+import { View, Picker, StyleSheet } from 'react-native';
+import Typography from "../Typography/Typography";
+
+
+class Select extends Component {
+
+    /**
+     * labelText: label on top left of the selector
+     * options: array of object with value and label fields
+     * **/
+
+    static propTypes = {
+        labelText: string,
+        options: array,
+        onValueChange: func,
+        selectedValue: string,
+        placeHolder: string,
+        cancelLabel: string,
+        type: string
+    };
+    static defaultProps = {
+        options: [],
+        onValueChange: () => {},
+        type: 'dark'
+    }
+
+    constructor (props) {
+        super(props);
+
+        let initialItems = Array.from(props.options);
+        initialItems.splice(0, 0, {value: '', label: props.placeHolder});
+
+        this.state = {
+            items: initialItems
+        }
+    }
+
+    componentWillReceiveProps (nextProps) {
+        const {options, placeHolder} = nextProps
+
+        let items = Array.from(options);
+        items.splice(0, 0, {value: '', label: placeHolder});
+
+        this.setState({items: items})
+    }
+
+    render () {
+        const {labelText, selectedValue, type, onValueChange} = this.props;
+        const {items} = this.state
+
+        return (
+            <View>
+                <Typography style={s.textContainer} type={type !== 'dark' ? 'darkText' : 'text'}>
+                    {labelText}
+                </Typography>
+                <View style={type === 'dark' ? s.valueContainerDark : s.valueContainer}>
+                    <Picker
+                        onValueChange={onValueChange}
+                        selectedValue={selectedValue}
+                        mode='dialog'
+                        style={{color: 'white'}}
+                    >
+                        {
+                            items.map((item, index) =>
+                                <Picker.Item
+                                    value={item.value}
+                                    label={item.label}
+                                    key={index}
+                                />)
+                        }
+                    </Picker>
+                </View>
+            </View>
+        );
+
+    }
+}
+
+const s = StyleSheet.create({
+    valueContainerDark: {
+        padding: 0,
+        height: 44,
+        borderRadius: 4,
+        backgroundColor: 'rgba(29, 28, 50, 0.4)',
+        justifyContent: 'space-around',
+    },
+    valueContainer: {
+        padding: 0,
+        height: 44,
+        borderRadius: 4,
+        backgroundColor: 'rgba(29, 28, 50, 0.4)',
+        justifyContent: 'space-around',
+    },
+    textContainer: {
+        paddingLeft: 12,
+        paddingBottom: 5,
+        fontSize: 12
+    }
+});
+
+
+export default Select;
---
+++ src/Select/Select.ios.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+
+import { string, array, func } from 'prop-types';
+
+import { View, TouchableOpacity, ActionSheetIOS, StyleSheet } from 'react-native';
+import Typography from "../Typography/Typography";
+
+
+class Select extends Component {
+
+    /**
+     * labelText: label on top left of the selector
+     * options: array of object with value and label fields
+     * **/
+
+    static propTypes = {
+        labelText: string,
+        options: array,
+        onValueChange: func,
+        selectedValue: string,
+        placeHolder: string,
+        cancelLabel: string,
+        type: string
+    };
+    static defaultProps = {
+        options: [],
+        onValueChange: () => {},
+        type: 'dark'
+    }
+
+    constructor (props) {
+        super(props);
+        this.onButtonPress = this.onButtonPress.bind(this);
+    }
+
+    onButtonPress () {
+        const {labelText, onValueChange, cancelLabel} = this.props;
+        let options = Array.from(this.props.options);
+        //@TODO is this a good idea?
+        options.push({value: '', label: cancelLabel});
+
+        ActionSheetIOS.showActionSheetWithOptions({
+            title: labelText,
+            cancelButtonIndex: options.length - 1,
+            options: options.map(opt => opt.label),
+        }, index => onValueChange(options[index].value))
+    }
+
+    render () {
+        const {labelText, selectedValue, options, placeHolder, type} = this.props;
+        let selected = options.find(el => el.value === selectedValue)
+        let label = selected ? selected.label : placeHolder;
+
+        return (
+            <View>
+                <Typography style={s.textContainer} type={type !== 'dark' ? 'darkText' : 'text'}>
+                    {labelText}
+                </Typography>
+                <TouchableOpacity onPress={this.onButtonPress}>
+                    <View style={type === 'dark' ? s.valueContainerDark : s.valueContainer}>
+                        <Typography style={{marginLeft: 12}} type={type !== 'dark' ? 'darkText' : 'text'}>
+                            {label}
+                        </Typography>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        );
+
+    }
+}
+
+const s = StyleSheet.create({
+    valueContainerDark: {
+        padding: 0,
+        height: 44,
+        borderRadius: 4,
+        backgroundColor: 'rgba(29, 28, 50, 0.4)',
+        justifyContent: 'space-around',
+    },
+    valueContainer: {
+        padding: 0,
+        height: 44,
+        borderRadius: 4,
+        backgroundColor: 'rgba(29, 28, 50, 0.4)',
+        justifyContent: 'space-around',
+    },
+    textContainer: {
+        paddingLeft: 12,
+        paddingBottom: 5,
+        fontSize: 12
+    }
+});
+
+
+export default Select;
