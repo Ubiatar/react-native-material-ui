1bff40d2e7496d2aa5cff63d16a295e81cc11417
Antonio <antonio.rinaldo89@gmail.com>
2018-09-14 12:30:21 +0200

    background image on chat added

--- src/GiftedChat/src/MessageContainer.js
+++ src/GiftedChat/src/MessageContainer.js
@@ -10,176 +10,181 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { FlatList, View, StyleSheet, Keyboard } from 'react-native';
+import { FlatList, View, StyleSheet, Keyboard, ImageBackground } from 'react-native';
 
 import LoadEarlier from './LoadEarlier';
 import Message from './Message';
 
 export default class MessageContainer extends React.PureComponent {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.renderRow = this.renderRow.bind(this);
-    this.renderFooter = this.renderFooter.bind(this);
-    this.renderLoadEarlier = this.renderLoadEarlier.bind(this);
-    this.renderHeaderWrapper = this.renderHeaderWrapper.bind(this);
-    this.attachKeyboardListeners = this.attachKeyboardListeners.bind(this);
-    this.detatchKeyboardListeners = this.detatchKeyboardListeners.bind(this);
+        this.renderRow = this.renderRow.bind(this);
+        this.renderFooter = this.renderFooter.bind(this);
+        this.renderLoadEarlier = this.renderLoadEarlier.bind(this);
+        this.renderHeaderWrapper = this.renderHeaderWrapper.bind(this);
+        this.attachKeyboardListeners = this.attachKeyboardListeners.bind(this);
+        this.detatchKeyboardListeners = this.detatchKeyboardListeners.bind(this);
 
-    if (props.messages.length === 0) {
-      this.attachKeyboardListeners(props);
+        if (props.messages.length === 0) {
+            this.attachKeyboardListeners(props);
+        }
     }
-  }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.messages.length === 0 && nextProps.messages.length > 0) {
-      this.detatchKeyboardListeners();
-    } else if (this.props.messages.length > 0 && nextProps.messages.length === 0) {
-      this.attachKeyboardListeners(nextProps);
+    componentWillReceiveProps(nextProps) {
+        if (this.props.messages.length === 0 && nextProps.messages.length > 0) {
+            this.detatchKeyboardListeners();
+        } else if (this.props.messages.length > 0 && nextProps.messages.length === 0) {
+            this.attachKeyboardListeners(nextProps);
+        }
     }
-  }
 
-  attachKeyboardListeners(props) {
-    Keyboard.addListener('keyboardWillShow', props.invertibleScrollViewProps.onKeyboardWillShow);
-    Keyboard.addListener('keyboardDidShow', props.invertibleScrollViewProps.onKeyboardDidShow);
-    Keyboard.addListener('keyboardWillHide', props.invertibleScrollViewProps.onKeyboardWillHide);
-    Keyboard.addListener('keyboardDidHide', props.invertibleScrollViewProps.onKeyboardDidHide);
-  }
-
-  detatchKeyboardListeners() {
-    Keyboard.removeListener('keyboardWillShow', this.props.invertibleScrollViewProps.onKeyboardWillShow);
-    Keyboard.removeListener('keyboardDidShow', this.props.invertibleScrollViewProps.onKeyboardDidShow);
-    Keyboard.removeListener('keyboardWillHide', this.props.invertibleScrollViewProps.onKeyboardWillHide);
-    Keyboard.removeListener('keyboardDidHide', this.props.invertibleScrollViewProps.onKeyboardDidHide);
-  }
-
-  renderFooter() {
-    if (this.props.renderFooter) {
-      const footerProps = {
-        ...this.props,
-      };
-      return this.props.renderFooter(footerProps);
+    attachKeyboardListeners(props) {
+        Keyboard.addListener('keyboardWillShow', props.invertibleScrollViewProps.onKeyboardWillShow);
+        Keyboard.addListener('keyboardDidShow', props.invertibleScrollViewProps.onKeyboardDidShow);
+        Keyboard.addListener('keyboardWillHide', props.invertibleScrollViewProps.onKeyboardWillHide);
+        Keyboard.addListener('keyboardDidHide', props.invertibleScrollViewProps.onKeyboardDidHide);
     }
-    return null;
-  }
 
-  renderLoadEarlier() {
-    if (this.props.loadEarlier === true) {
-      const loadEarlierProps = {
-        ...this.props,
-      };
-      if (this.props.renderLoadEarlier) {
-        return this.props.renderLoadEarlier(loadEarlierProps);
-      }
-      return <LoadEarlier {...loadEarlierProps} />;
+    detatchKeyboardListeners() {
+        Keyboard.removeListener('keyboardWillShow', this.props.invertibleScrollViewProps.onKeyboardWillShow);
+        Keyboard.removeListener('keyboardDidShow', this.props.invertibleScrollViewProps.onKeyboardDidShow);
+        Keyboard.removeListener('keyboardWillHide', this.props.invertibleScrollViewProps.onKeyboardWillHide);
+        Keyboard.removeListener('keyboardDidHide', this.props.invertibleScrollViewProps.onKeyboardDidHide);
     }
-    return null;
-  }
 
-  scrollTo(options) {
-    if (this.flatListRef) {
-      this.flatListRef.scrollToOffset(options);
+    renderFooter() {
+        if (this.props.renderFooter) {
+            const footerProps = {
+                ...this.props,
+            };
+            return this.props.renderFooter(footerProps);
+        }
+        return null;
     }
-  }
 
-  renderRow({ item, index }) {
-    if (!item._id && item._id !== 0) {
-      console.warn('GiftedChat: `_id` is missing for message', JSON.stringify(item));
+    renderLoadEarlier() {
+        if (this.props.loadEarlier === true) {
+            const loadEarlierProps = {
+                ...this.props,
+            };
+            if (this.props.renderLoadEarlier) {
+                return this.props.renderLoadEarlier(loadEarlierProps);
+            }
+            return <LoadEarlier {...loadEarlierProps} />;
+        }
+        return null;
     }
-    if (!item.user) {
-      if (!item.system) {
-        console.warn('GiftedChat: `user` is missing for message', JSON.stringify(item));
-      }
-      item.user = {};
+
+    scrollTo(options) {
+        if (this.flatListRef) {
+            this.flatListRef.scrollToOffset(options);
+        }
     }
-    const { messages, ...restProps } = this.props;
-    const previousMessage = messages[index + 1] || {};
-    const nextMessage = messages[index - 1] || {};
 
-    const messageProps = {
-      ...restProps,
-      key: item._id,
-      currentMessage: item,
-      previousMessage,
-      nextMessage,
-      position: item.user._id === this.props.user._id ? 'right' : 'left',
-    };
+    renderRow({ item, index }) {
+        if (!item._id && item._id !== 0) {
+            console.warn('GiftedChat: `_id` is missing for message', JSON.stringify(item));
+        }
+        if (!item.user) {
+            if (!item.system) {
+                console.warn('GiftedChat: `user` is missing for message', JSON.stringify(item));
+            }
+            item.user = {};
+        }
+        const { messages, ...restProps } = this.props;
+        const previousMessage = messages[index + 1] || {};
+        const nextMessage = messages[index - 1] || {};
 
-    if (this.props.renderMessage) {
-      return this.props.renderMessage(messageProps);
+        const messageProps = {
+            ...restProps,
+            key: item._id,
+            currentMessage: item,
+            previousMessage,
+            nextMessage,
+            position: item.user._id === this.props.user._id ? 'right' : 'left',
+        };
+
+        if (this.props.renderMessage) {
+            return this.props.renderMessage(messageProps);
+        }
+        return <Message {...messageProps} />;
     }
-    return <Message {...messageProps} />;
-  }
 
-  renderHeaderWrapper() {
-    return <View style={styles.headerWrapper}>{this.renderLoadEarlier()}</View>;
-  }
-
-  render() {
-    if (this.props.messages.length === 0) {
-      return <View style={styles.container} />;
+    renderHeaderWrapper() {
+        return <View style={styles.headerWrapper}>{this.renderLoadEarlier()}</View>;
     }
-    return (
-      <View style={styles.container}>
-        <FlatList
-          ref={(ref) => (this.flatListRef = ref)}
-          keyExtractor={(item) => item._id}
-          enableEmptySections
-          automaticallyAdjustContentInsets={false}
-          inverted={this.props.inverted}
-          data={this.props.messages}
-          style={styles.listStyle}
-          contentContainerStyle={styles.contentContainerStyle}
-          renderItem={this.renderRow}
-          {...this.props.invertibleScrollViewProps}
-          ListFooterComponent={this.renderHeaderWrapper}
-          ListHeaderComponent={this.renderFooter}
-          {...this.props.listViewProps}
-        />
-      </View>
-    );
-  }
+
+    render() {
+        if (this.props.messages.length === 0) {
+            return <View style={styles.container} >
+                <ImageBackground style={{width: '100%', height: '100%'}} {...this.props.image} />
+            </View>;
+        }
+        return (
+            <View style={styles.container}>
+                <ImageBackground style={{width: '100%', height: '100%'}} {...this.props.image} >
+                    <FlatList
+                        ref={(ref) => (this.flatListRef = ref)}
+                        keyExtractor={(item) => item._id}
+                        enableEmptySections
+                        automaticallyAdjustContentInsets={false}
+                        inverted={this.props.inverted}
+                        data={this.props.messages}
+                        style={styles.listStyle}
+                        contentContainerStyle={styles.contentContainerStyle}
+                        renderItem={this.renderRow}
+                        {...this.props.invertibleScrollViewProps}
+                        ListFooterComponent={this.renderHeaderWrapper}
+                        ListHeaderComponent={this.renderFooter}
+                        {...this.props.listViewProps}
+                    />
+                </ImageBackground>
+            </View>
+        );
+    }
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingBottom: 15
-  },
-  contentContainerStyle: {
-    justifyContent: 'flex-end',
-  },
-  headerWrapper: {
-    flex: 1,
-  },
-  listStyle: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+        paddingBottom: 15
+    },
+    contentContainerStyle: {
+        justifyContent: 'flex-end',
+    },
+    headerWrapper: {
+        flex: 1,
+    },
+    listStyle: {
+        flex: 1,
+    },
 });
 
 MessageContainer.defaultProps = {
-  messages: [],
-  user: {},
-  renderFooter: null,
-  renderMessage: null,
-  onLoadEarlier: () => {},
-  inverted: true,
-  loadEarlier: false,
-  listViewProps: {},
-  invertibleScrollViewProps: {}, // TODO: support or not?
+    messages: [],
+    user: {},
+    renderFooter: null,
+    renderMessage: null,
+    onLoadEarlier: () => {},
+    inverted: true,
+    loadEarlier: false,
+    listViewProps: {},
+    invertibleScrollViewProps: {}, // TODO: support or not?
 };
 
 MessageContainer.propTypes = {
-  messages: PropTypes.arrayOf(PropTypes.object),
-  user: PropTypes.object,
-  renderFooter: PropTypes.func,
-  renderMessage: PropTypes.func,
-  renderLoadEarlier: PropTypes.func,
-  onLoadEarlier: PropTypes.func,
-  listViewProps: PropTypes.object,
-  inverted: PropTypes.bool,
-  loadEarlier: PropTypes.bool,
-  invertibleScrollViewProps: PropTypes.object, // TODO: support or not?
+    messages: PropTypes.arrayOf(PropTypes.object),
+    user: PropTypes.object,
+    renderFooter: PropTypes.func,
+    renderMessage: PropTypes.func,
+    renderLoadEarlier: PropTypes.func,
+    onLoadEarlier: PropTypes.func,
+    listViewProps: PropTypes.object,
+    image: PropTypes.object,
+    inverted: PropTypes.bool,
+    loadEarlier: PropTypes.bool,
+    invertibleScrollViewProps: PropTypes.object, // TODO: support or not?
 };
