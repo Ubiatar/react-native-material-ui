3203d2d612cee290f8e4736d7045ea1acd4b3c18
androsanta <andreasantu@gmail.com>
2018-09-27 14:22:25 +0200

    textfield refactoring

--- src/TextField/index.js
+++ src/TextField/index.js
@@ -1,1 +1,23 @@
-export { default } from './TextField';
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { StyleSheet } from 'react-native';
+import TextField from './field';
+
+const contextTypes = {
+    uiTheme: PropTypes.object.isRequired,
+};
+
+
+export default class CustomTextField extends PureComponent {
+    render() {
+        // const otherProps = getStyles(this.props, this.context);
+        return (
+            <TextField
+                {...StyleSheet.flatten(this.context.uiTheme.textfield)}
+                {...this.props}
+            />
+        );
+    }
+}
+
+CustomTextField.contextTypes = contextTypes;
